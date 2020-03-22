cbec09adeb08cd2ed84fb1fcbac021bb382b9bfe
Vinícius Estevam <estevamvinicius31@gmail.com>
2020-03-22 01:56:41 -0300

    fix: change to localhost and add readme

---
+++ README.md
@@ -0,0 +1,4 @@
+<div align="center">
+  <img src="https://imgur.com/s3XNbCr"/>
+</div>
+<h1>Twitter clone</h1>
--- frontend/src/config/graphql-config.js
+++ frontend/src/config/graphql-config.js
@@ -6,11 +6,11 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
 
 const httpLink = new HttpLink({
-  uri: 'http://192.168.0.108:8000/graphql'
+  uri: 'http://localhost:8000/graphql'
 });
 
 const wsLink = new WebSocketLink({
-  uri: `ws://192.168.0.108:8000/graphql`,
+  uri: `ws://localhost:8000/graphql`,
   options: {
     reconnect: true
   }
