94c39ef15d9fbe57194eed6635d1dc99d9558b25
Vinícius Estevam <estevamvinicius31@gmail.com>
2020-03-23 11:25:36 -0300

    Updates

---
+++ precache-manifest.37ce331f0dde07e6bfef72bf70e1a4fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51468ca80d33bae074595faa7b62bde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bb36df4f43e736c94cb",
+    "url": "/static/css/main.c0a62001.chunk.css"
+  },
+  {
+    "revision": "c750ad9d8207aff86a1c",
+    "url": "/static/js/2.eddb1328.chunk.js"
+  },
+  {
+    "revision": "d94d8d6c9f26b9d161f002f805e91262",
+    "url": "/static/js/2.eddb1328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb36df4f43e736c94cb",
+    "url": "/static/js/main.3593686e.chunk.js"
+  },
+  {
+    "revision": "3484b2f6dd70a9599247",
+    "url": "/static/js/runtime-main.79fa3fc6.js"
+  }
+]);
